540e24faf649fa3c0b250b1dca979d66ca4fd700
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 21:49:57 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.28-1-MANJARO #1 SMP PREEMPT Fri Mar 11 14:12:57 UTC 2022 x86_64 GNU/Linux  21:49:57 up  6:38,  1 user,  load average: 0.61, 0.43, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,14 +103,17 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
         int offset = 0;
+        int len = substr_len;
         switch (rules[i].token_type) {
           case TK_NOTYPE: break;
           case TK_HEX:
             offset++;  // strip prefix $
+            len--;
           case TK_REG:
             offset++;  // strip prefix 0x/0X
+            len--;
           case TK_DEC:
-            strncpy(tokens[nr_token].str, substr_start + offset, substr_len);
+            strncpy(tokens[nr_token].str, substr_start + offset, len);
           default:
             tokens[nr_token].type = rules[i].token_type;
             nr_token++;
