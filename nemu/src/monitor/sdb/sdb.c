0eaa7d48611cb2b08bc785011bd15c16e27a4c63
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-13 21:37:12 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.28-1-MANJARO #1 SMP PREEMPT Fri Mar 11 14:12:57 UTC 2022 x86_64 GNU/Linux  21:37:12 up  6:35,  1 user,  load average: 1.39, 1.39, 1.30

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -80,9 +80,8 @@ static int cmd_x(char *args) {
   } else {
     int N = strtol(arg, NULL, 10);
     printf("%lu", strlen(arg));
-    printf(">>>%s<<<", args);
-    args += strlen(arg);
-    printf(">>>%s<<<", args);
+    arg += strlen(arg) + 1;
+    printf(">>>%s<<<", arg);
     // TODO: only accept hex number for now
     bool success;
     word_t expression = expr(args, &success);
