55a98f1438af03d20f38390fce415ae6f1d823a8
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-14 17:28:34 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.28-1-MANJARO #1 SMP PREEMPT Fri Mar 11 14:12:57 UTC 2022 x86_64 GNU/Linux  17:28:34 up  6:45,  1 user,  load average: 1.56, 1.01, 0.79

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -3,10 +3,10 @@
 #define NR_WP 32 // number range of watchpoint
 
 typedef struct watchpoint {
-  int NO;                  // number of watchpoint
-  struct watchpoint *next; // pointer to next element
+    int NO;                  ///< number of watchpoint
+    struct watchpoint *next; ///< pointer to next element
 
-  /* TODO: Add more members if necessary */
+    /* TODO: Add more members if necessary */
 
 } WP;
 
@@ -14,20 +14,20 @@ static WP wp_pool[NR_WP] = {};
 static WP *head = NULL;  // watchpoints in use
 static WP *free_ = NULL; // idle watchpoints
 
-// This will initialize head and free_
+/// @brief initialize head and free_
 void init_wp_pool() {
-  int i;
-  for (i = 0; i < NR_WP; i++) {
-    wp_pool[i].NO = i;
-    wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
-  }
+    int i;
+    for (i = 0; i < NR_WP; i++) {
+        wp_pool[i].NO = i;
+        wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    }
 
-  head = NULL;
-  free_ = wp_pool;
+    head = NULL;
+    free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
-///
-/// \return
+
 WP *new_wp();
+
 void free_wp(WP *wp);
