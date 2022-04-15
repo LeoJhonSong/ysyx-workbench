30d83cdf8595864e638f591f0a0ab312eb6c7755
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-15 20:14:33 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.28-1-MANJARO #1 SMP PREEMPT Fri Mar 11 14:12:57 UTC 2022 x86_64 GNU/Linux  20:14:33 up  7:12,  1 user,  load average: 0.97, 1.09, 1.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -1,5 +1,7 @@
 #include "debug.h"
 #include "sdb.h"
+#include "utils.h"
+
 #include <stdio.h>
 
 #define NR_WP 32 // number range of watchpoint
@@ -7,6 +9,7 @@
 static struct watchpoint wp_pool[NR_WP] = {};
 static wp_link head = NULL;  // watchpoints in use
 static wp_link free_ = NULL; // idle watchpoints
+static int wps_in_use = 0;   // amount of watchpoint in use
 
 /// @brief initialize head and free_
 void init_wp_pool() {
@@ -23,42 +26,49 @@ void init_wp_pool() {
 /* TODO: Implement the functionality of watchpoint */
 
 ///
-///@brief Get a new watchpoint from free_. When no more idle watchpoint in free_, Assert(0) triggered
+///@brief Pop a watchpoint from free_ and push to head. When no more idle watchpoint in free_, Assert(0) triggered
 ///
-///@return WP* The first watchpoint from free_
+///@return WP* The first watchpoint from free_, also the head
 ///
 wp_link new_wp() {
     wp_link p = free_;
     free_ = free_->next;
     p->next = head;
     head = p;
+    wps_in_use++;
     return head;
 }
 
 ///
-///@brief Free a watchpoint from head to free_
+///@brief Pop a watchpoint from head and push to free_
 ///
-///@param idx The index of the watchpoint in head
+///@param idx The index of the watchpoint in use to pop
 ///
 void free_wp_by_idx(int idx) {
-    Assert(idx >= 0 && idx < NR_WP, "index %d out of range of 0<= idx < %d", idx, NR_WP);
-    wp_link p = head;
-    if (idx == 0) {
-        head = head->next;
-        p->next = free_;
-        free_ = p;
+    if (idx < 0 || idx >= NR_WP) {
+        ERROR("index %d out of range of 0<= idx <= %d", idx, wps_in_use - 1);
     } else {
-        for (int i = 0; i < idx - 1; i++) {
-            p = p->next;
+        wp_link p = head;
+        if (idx == 0) {
+            head = head->next;
+            p->next = free_;
+            free_ = p;
+        } else {
+            for (int i = 0; i < idx - 1; i++) {
+                p = p->next;
+            }
+            // delete p->next from watchpoints in use and push to free_
+            wp_link rest = p->next->next;
+            p->next->next = free_;
+            free_ = p->next;
+            p->next = rest;
         }
-        // delete p->next from watchpoints in use and push to free_
-        wp_link rest = p->next->next;
-        p->next->next = free_;
-        free_ = p->next;
-        p->next = rest;
     }
 }
 
+///
+///@brief Traverse from the head and print info of each watchpoint
+///
 void print_wps() {
     int i = 0;
     for (wp_link p = head; p; p = p->next) {
