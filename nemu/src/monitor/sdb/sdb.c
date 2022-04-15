8248c36d0378fad0e5e92c034ea0c5e953d5480a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-15 20:38:41 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.28-1-MANJARO #1 SMP PREEMPT Fri Mar 11 14:12:57 UTC 2022 x86_64 GNU/Linux  20:38:41 up  7:37,  1 user,  load average: 0.89, 1.13, 1.07

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -110,6 +110,7 @@ static int cmd_p(char *args) {
 }
 
 static int cmd_w(char *args) {
+    args += strlen(args) + 1; // +1 to skip the \0
     new_wp(args);
     // printf(">>>%d<<<\n", wp->NO);
     return 0;
