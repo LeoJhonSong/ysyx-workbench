f09e7ed67769ac7b9c793ebfd76162b0cad0fb25
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-16 09:25:12 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  09:25:12 up 59 min,  1 user,  load average: 1.22, 1.09, 1.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -27,9 +27,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
     if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
     IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-    if (CONFIG_WATCHPOINT) {
-        if (wps_check()) { nemu_state.state = NEMU_STOP; } // once value of watchpoints change, stop nemu and return to sdb_mainloop()
-    }
+#ifdef CONFIG_WATCHPOINT
+    if (wps_check()) { nemu_state.state = NEMU_STOP; } // once value of watchpoints change, stop nemu and return to sdb_mainloop()
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
