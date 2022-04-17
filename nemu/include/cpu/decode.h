925091484847c2f97d55495d1abb7e5b9b0f1fe2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 17:31:52 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  17:31:52 up 1 day,  9:05,  1 user,  load average: 1.94, 2.27, 2.43

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -22,7 +22,7 @@ __attribute__((always_inline)) static inline void pattern_decode(const char *str
         goto finish;                                                                                 \
     else {                                                                                           \
         char c = str[i];                                                                             \
-        if (c != ' ' && c!= '│') {                                                                              \
+        if (c != ' ' && c!= '4') {                                                                              \
             Assert(c == '0' || c == '1' || c == '?', "invalid character '%c' in pattern string", c); \
             __key = (__key << 1) | (c == '1' ? 1 : 0);                                               \
             __mask = (__mask << 1) | (c == '?' ? 0 : 1);                                             \
