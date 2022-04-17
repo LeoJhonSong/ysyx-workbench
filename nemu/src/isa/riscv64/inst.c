366c4b3511b03cedd2c81fd465a5a69d09dd6268
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-18 01:53:37 +0800

    > compile NEMU ysyx_22041024 宋铸恒 Linux Leo-AERO15WB 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  01:53:37 up 1 day, 17:27,  1 user,  load average: 1.25, 1.39, 1.70

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -31,12 +31,6 @@ enum {
 #define src2I(i) do { *src2_ptr = i; } while (0)
 #define destI(i) do { *dest_ptr = i; } while (0)
 
-// Get immediate value from instruction based on instruction type
-static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
-static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
-static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20 | SEXT(BITS(i, 30, 21), 10) << 1 | SEXT(BITS(i, 20, 20), 1) << 11 | SEXT(BITS(i, 19, 12), 8) << 12);}
-
 ///
 ///@brief Extract operands rd, rs1, rs2 from instruction s->isa.inst.val to vars dest, src1, src2
 ///
@@ -46,34 +40,38 @@ static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20 | SEXT(B
 ///@param src2 Stores operand \p rs2
 ///@param type Type of the instruction
 ///
-static void decode_operand(Decode *s, word_t *dest_ptr, word_t *src1_ptr, word_t *src2_ptr, int type) {
+static void decode_operand(Decode *s, word_t **rd_pptr, word_t **rs1_pptr, word_t **rs2_pptr, word_t *imm_ptr, int type) {
     uint32_t i = s->isa.inst.val;
     int rd = BITS(i, 11, 7);
     int rs1 = BITS(i, 19, 15);
     int rs2 = BITS(i, 24, 20);
-    destR(rd);
     switch (type) {
         case TYPE_R:
             TODO();
             break;
         case TYPE_I:
-            src1R(rs1);
-            src2I(immI(i));
+            *rd_pptr = &R(rd);
+            *rs1_pptr = &R(rs1);
+            *imm_ptr = SEXT(BITS(i, 31, 20), 12);
             break;
         case TYPE_S:
-            destI(immS(i));
-            src1R(rs1);
-            src2R(rs2);
+            *rs1_pptr = &R(rs1);
+            *rs2_pptr = &R(rs2);
+            *imm_ptr = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7);
             break;
         case TYPE_B:
             TODO();
             break;
         case TYPE_U:
-            src1I(immU(i));
+            *rd_pptr = &R(rd);
+            *imm_ptr = SEXT(BITS(i, 31, 12), 20) << 12;
             break;
         case TYPE_J:
-            TODO();
+            *rd_pptr = &R(rd);
+            *imm_ptr = (SEXT(BITS(i, 31, 31), 1) << 20 | SEXT(BITS(i, 30, 21), 10) << 1 | SEXT(BITS(i, 20, 20), 1) << 11 | SEXT(BITS(i, 19, 12), 8) << 12);
             break;
+        default:
+            Assert(0, "Error instruction type\n");
     }
 }
 
@@ -84,6 +82,11 @@ static void decode_operand(Decode *s, word_t *dest_ptr, word_t *src1_ptr, word_t
 ///
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 
+#define rd *rd_ptr
+#define rs1 *rs1_ptr
+#define rs2 *rs2_ptr
+#define PC s->pc
+
 ///
 ///@brief Execute operation for current instruction
 ///
@@ -92,10 +95,10 @@ static void decode_operand(Decode *s, word_t *dest_ptr, word_t *src1_ptr, word_t
 ///@param name Name of the instruction
 ///@param body Operation of the instruction
 ///
-#define INSTPAT_MATCH(s, type, name, ... /* body */)                 \
-    {                                                                \
-        decode_operand(s, &dest, &src1, &src2, concat(TYPE_, type)); \
-        __VA_ARGS__;                                                 \
+#define INSTPAT_MATCH(s, type, name, ... /* body */)                               \
+    {                                                                              \
+        decode_operand(s, &rd_ptr, &rs1_ptr, &rs2_ptr, &imm, concat(TYPE_, type)); \
+        __VA_ARGS__;                                                               \
     }
 
 ///
@@ -105,8 +108,7 @@ static void decode_operand(Decode *s, word_t *dest_ptr, word_t *src1_ptr, word_t
 ///@return Always return 0, useless for now
 ///
 static int decode_exec(Decode *s) {
-    word_t dest = 0, src1 = 0, src2 = 0;
-    word_t rd, rs1, rs2, imm = 0;
+    word_t rd, rs1, rs2, imm;
 
     s->dnpc = s->snpc; // For normal instructions, dynamic next pc should be same with staticc next pc
 
@@ -118,15 +120,15 @@ static int decode_exec(Decode *s) {
     // RV32I/RV64I Base Integer Instructions
     // R-type │funct7      │rs2  │rs1  │funct3│rd         │opcode │
     // I-type │imm[11:0]         │rs1  │funct3│rd         │opcode │
-    INSTPAT(L"│????????????      │?????│000   │?????      │1100111│", I, jalr, TODO());
-    INSTPAT(L"│????????????      │?????│011   │?????      │0000011│", I, ld, R(dest) = Mr(src1 + src2, 8));
+    INSTPAT(L"│????????????      │?????│000   │?????      │1100111│", I, jalr, TODO()); //TODO: remove name
+    INSTPAT(L"│????????????      │?????│011   │?????      │0000011│", I, ld, rd = Mr(rs1 + imm, 8));
     INSTPAT(L"│????????????      │?????│000   │?????      │0010011│", I, addi, TODO());
-    INSTPAT(L"│000000000001      │00000│000   │00000      │1110011│", I, ebreak, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+    INSTPAT(L"│000000000001      │00000│000   │00000      │1110011│", I, ebreak, NEMUTRAP(PC, R(10))); // R(10) is $a0
     // S-type │imm[11:5]   │rs2  │rs1  │funct3│imm[4:0]   │opcode │
-    INSTPAT(L"│???????     │?????│?????│011   │?????      │0100011│", S, sd, Mw(src1 + dest, 8, src2));
+    INSTPAT(L"│???????     │?????│?????│011   │?????      │0100011│", S, sd, Mw(rs1 + imm, 8, rs2));
     // B-type │imm[12|10:5]│rs2  │rs1  │funct3│imm[4:1|11]│opcode │
     // U-type │imm[31:12]                     │rd         │opcode │
-    INSTPAT(L"│????????????????????           │?????      │0010111│", U, auipc, R(dest) = src1 + s->pc);
+    INSTPAT(L"│????????????????????           │?????      │0010111│", U, auipc, rd = PC + imm);
     // J-type │imm[20|10:1|11|19:12]          │rd         │opcode │
     INSTPAT(L"│????????????????????           │?????      │1101111│", J, jal, TODO());
 
@@ -139,6 +141,10 @@ static int decode_exec(Decode *s) {
 
     return 0;
 }
+#undef rd
+#undef rs1
+#undef rs2
+#undef PC
 
 int isa_exec_once(Decode *s) {
     s->isa.inst.val = inst_fetch(&s->snpc, 4);
