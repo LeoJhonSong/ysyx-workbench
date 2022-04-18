95b7971ecbdf45080abeb1e88089b7fe4653dec6
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-18 14:48:03 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  14:48:03 up 10:30,  0 users,  load average: 1.80, 1.95, 1.67

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -24,7 +24,6 @@ __attribute__((noinline)) void invalid_inst(vaddr_t thispc) {
     char opcode[8];
     char *opcode_ptr = opcode;
     for (int i = 6; i >= 0; i--) { opcode_ptr += snprintf(opcode_ptr, ARRLEN(opcode), "%d", ((inst & 0b1111111) >> i) & 0b1); }
-    printf("%x\n", inst & 0b1111111);
     printf(ANSI_FMT(" Invalid instruction!\n", ANSI_FG_YELLOW));
     printf("assemble:\t" ANSI_FMT("%s\n", ANSI_FG_RED), assemble_str);
     printf("opcode:\t\t" ANSI_FMT("%s\n", ANSI_FG_NORMAL_GREEN), opcode);
