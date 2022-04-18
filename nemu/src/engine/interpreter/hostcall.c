60a271d76be3a7d6c89b72f21ad3363b86c81379
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-18 14:40:07 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  14:40:07 up 10:22,  0 users,  load average: 2.72, 1.71, 1.36

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -8,39 +8,39 @@
 #include <utils.h>
 
 void set_nemu_state(int state, vaddr_t pc, int halt_ret) {
-  difftest_skip_ref();
-  nemu_state.state = state;
-  nemu_state.halt_pc = pc;
-  nemu_state.halt_ret = halt_ret;
+    difftest_skip_ref();
+    nemu_state.state = state;
+    nemu_state.halt_pc = pc;
+    nemu_state.halt_ret = halt_ret;
 }
 
-__attribute__((noinline))
-void invalid_inst(vaddr_t thispc) {
-  vaddr_t pc = thispc;
-  uint32_t inst = inst_fetch(&pc, 4);
+__attribute__((noinline)) void invalid_inst(vaddr_t thispc) {
+    vaddr_t pc = thispc;
+    uint32_t inst = inst_fetch(&pc, 4);
 
-  char assemble_str[128];
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(assemble_str, 128, thispc, (uint8_t *)&inst, 4);
-  char opcode[8];
-  char *opcode_ptr = opcode;
-  for (int i = 6; i >= 0; i--) {
-    opcode_ptr += snprintf(opcode_ptr, ARRLEN(opcode), "%d", ((inst & 0b1111111) >> i) & 0b1);
-  }
-  printf("%x\n", inst & 0b1111111);
-  printf(ANSI_FMT(" Invalid instruction!\n", ANSI_FG_YELLOW));
-  printf("assemble:\t" ANSI_FMT("%s\n", ANSI_FG_RED), assemble_str);
-  printf("opcode:\t\t" ANSI_FMT("%s\n", ANSI_FG_NORMAL_GREEN), opcode);
-  printf("PC:\t\t" ANSI_FMT(FMT_WORD "\n", ANSI_FG_WHITE), thispc);
+    char assemble_str[128];
+    void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+    disassemble(assemble_str, 128, thispc, (uint8_t *)&inst, 4);
+    char opcode[8];
+    char *opcode_ptr = opcode;
+    for (int i = 6; i >= 0; i--) { opcode_ptr += snprintf(opcode_ptr, ARRLEN(opcode), "%d", ((inst & 0b1111111) >> i) & 0b1); }
+    printf("%x\n", inst & 0b1111111);
+    printf(ANSI_FMT(" Invalid instruction!\n", ANSI_FG_YELLOW));
+    printf("assemble:\t" ANSI_FMT("%s\n", ANSI_FG_RED), assemble_str);
+    printf("opcode:\t\t" ANSI_FMT("%s\n", ANSI_FG_NORMAL_GREEN), opcode);
+    printf("PC:\t\t" ANSI_FMT(FMT_WORD "\n", ANSI_FG_WHITE), thispc);
 
-  // printf("There are two cases which will trigger this unexpected exception:\n"
-  //     "1. The instruction at PC = " FMT_WORD " is not implemented.\n"
-  //     "2. Something is implemented incorrectly.\n", thispc);
-  // printf("Find this PC(" FMT_WORD ") in the disassembling result to distinguish which case it is.\n\n", thispc);
-  // printf(ANSI_FMT("If it is the first case, see\n%s\nfor more details.\n\n"
-  //       "If it is the second case, remember:\n"
-  //       "* The machine is always right!\n"
-  //       "* Every line of untested code is always wrong!\n\n", ANSI_FG_RED), isa_logo);
+    // printf("There are two cases which will trigger this unexpected exception:\n"
+    //        "1. The instruction at PC = " FMT_WORD " is not implemented.\n"
+    //        "2. Something is implemented incorrectly.\n",
+    //        thispc);
+    // printf("Find this PC(" FMT_WORD ") in the disassembling result to distinguish which case it is.\n\n", thispc);
+    // printf(ANSI_FMT("If it is the first case, see\n%s\nfor more details.\n\n"
+    //                 "If it is the second case, remember:\n"
+    //                 "* The machine is always right!\n"
+    //                 "* Every line of untested code is always wrong!\n\n",
+    //                 ANSI_FG_RED),
+    //        isa_logo);
 
-  set_nemu_state(NEMU_ABORT, thispc, -1);
+    set_nemu_state(NEMU_ABORT, thispc, -1);
 }
