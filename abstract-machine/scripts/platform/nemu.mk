b835e00d52ccdb0f83a8c8984fa5db7808fe890d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 04:25:23 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  04:25:23 up 1 day, 7 min,  0 users,  load average: 2.03, 1.22, 1.09

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -23,7 +23,6 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	@printf "\033[1;36m $(NAME)\033[0m\n" >> /dev/tty
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
 
 gdb: image
