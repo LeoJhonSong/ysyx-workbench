e583b87dc1810f70d4f330dae091b848eb130f93
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 10:24:12 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  10:24:12 up 1 day,  6:06,  0 users,  load average: 1.39, 1.35, 1.34

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -1,4 +1,3 @@
-#include "debug.h"
 #include <utils.h>
 
 NEMUState nemu_state = { .state = NEMU_STOP };
@@ -6,6 +5,5 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-  _Log("[exit code] %d\n", !good);
   return !good;
 }
