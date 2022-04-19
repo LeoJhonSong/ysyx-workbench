c5c361f5b12b8a8798bb12b6bf3bed122db15671
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-19 06:39:56 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  06:39:56 up 1 day,  2:22,  0 users,  load average: 1.39, 1.02, 1.18

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -21,7 +21,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 	$(call git_commit, "run NEMU")
 	-$(NEMU_EXEC); \
-	echo exit code: $$ > $(BUILD_DIR)/nemu-log.txt
+	echo exit code: $$? >> $(BUILD_DIR)/nemu-log.txt
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
