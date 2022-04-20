3866bee902f258e8fbcf3077c54eb2a05e3e01f3
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-20 05:11:20 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  05:11:20 up  2:51,  0 users,  load average: 0.84, 0.61, 0.57

--- am-kernels/tests/cpu-tests/tests/add.c
+++ am-kernels/tests/cpu-tests/tests/add.c
@@ -17,7 +17,7 @@ int main() {
 	int i, j, ans_idx = 0;
 	for(i = 0; i < NR_DATA; i ++) {
 		for(j = i; j < NR_DATA; j ++) {
-			check(add(test_data[i], test_data[j]) == ans[i + ans_idx]);
+			check(add(test_data[i], test_data[j]) == ans[ans_idx]);
 			ans_idx++;
 		}
 	}
