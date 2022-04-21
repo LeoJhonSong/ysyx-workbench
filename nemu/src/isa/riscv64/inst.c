81e85a404cf82036c12f87598d0e02318d10663a
Leo-AERO15WB <LeoJhon.Song@outlook.com>
2022-04-22 04:04:51 +0800

    feat(ppa2.2): complete instructions required for cpu-tests

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -34,35 +34,39 @@ enum {
 ///@param imm_ptr Pointer to immediate value \p imm
 ///@param type Type of the instruction
 ///
-static void decode_operand(Decode *s, word_t **rd_pptr, word_t **rs1_pptr, word_t **rs2_pptr, word_t *imm_ptr, int type) {
+static void decode_operand(Decode *s, int64_t **rd_pptr, int64_t **rs1_pptr, int64_t **rs2_pptr, int64_t *imm_ptr, int type) {
     uint32_t i = s->isa.inst.val;
     int rd = BITS(i, 11, 7);
     int rs1 = BITS(i, 19, 15);
     int rs2 = BITS(i, 24, 20);
     switch (type) {
         case TYPE_R:
-            TODO();
+            *rd_pptr = (int64_t *)&R(rd);
+            *rs1_pptr = (int64_t *)&R(rs1);
+            *rs2_pptr = (int64_t *)&R(rs2);
             break;
         case TYPE_I:
-            *rd_pptr = &R(rd);
-            *rs1_pptr = &R(rs1);
+            *rd_pptr = (int64_t *)&R(rd);
+            *rs1_pptr = (int64_t *)&R(rs1);
             *imm_ptr = SEXT(BITS(i, 31, 20), 12);
             break;
         case TYPE_S:
-            *rs1_pptr = &R(rs1);
-            *rs2_pptr = &R(rs2);
-            *imm_ptr = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7);
+            *rs1_pptr = (int64_t *)&R(rs1);
+            *rs2_pptr = (int64_t *)&R(rs2);
+            *imm_ptr = SEXT(BITS(i, 31, 25), 7) << 5 | BITS(i, 11, 7);
             break;
         case TYPE_B:
-            TODO();
+            *rs1_pptr = (int64_t *)&R(rs1);
+            *rs2_pptr = (int64_t *)&R(rs2);
+            *imm_ptr = SEXT(BITS(i, 31, 31), 1) << 12 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1 | BITS(i, 7, 7) << 11;
             break;
         case TYPE_U:
-            *rd_pptr = &R(rd);
-            *imm_ptr = SEXT(BITS(i, 31, 12), 20) << 12;
+            *rd_pptr = (int64_t *)&R(rd);
+            *imm_ptr = SEXT(BITS(i, 31, 12) << 12, 32); // sext(imm[31:12]) << 12
             break;
         case TYPE_J:
-            *rd_pptr = &R(rd);
-            *imm_ptr = (SEXT(BITS(i, 31, 31), 1) << 20 | SEXT(BITS(i, 30, 21), 10) << 1 | SEXT(BITS(i, 20, 20), 1) << 11 | SEXT(BITS(i, 19, 12), 8) << 12);
+            *rd_pptr = (int64_t *)&R(rd);
+            *imm_ptr = SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 30, 21) << 1 | BITS(i, 20, 20) << 11 | BITS(i, 19, 12) << 12;
             break;
         default:
             Assert(0, "Error instruction type\n");
@@ -100,7 +104,7 @@ static void decode_operand(Decode *s, word_t **rd_pptr, word_t **rs1_pptr, word_
 ///@return Always return 0, useless for now
 ///
 static int decode_exec(Decode *s) {
-    word_t rd, rs1, rs2, imm;
+    int64_t rd, rs1, rs2, imm;
 
     s->dnpc = s->snpc; // For normal instructions, dynamic next pc should be same with staticc next pc
 
@@ -109,20 +113,68 @@ static int decode_exec(Decode *s) {
     // Note: only 0/1/? are allowed in pattern string, 0/1 only matches 0/1, ? matches 0 or 1
     // spaces, │ in pattern string are ignored
 
-    // RV32I/RV64I Base Integer Instructions
+    // RV64I Base Integer Instructions
     // R-type │funct7      │rs2  │rs1  │funct3│rd         │opcode │
+    INSTPAT(L"│0000000     │?????│?????│000   │?????      │0110011│", R, rd = rs1 + rs2);                                  // add
+    INSTPAT(L"│0100000     │?????│?????│000   │?????      │0110011│", R, rd = rs1 - rs2);                                  // sub
+    INSTPAT(L"│0000000     │?????│?????│001   │?????      │0110011│", R, rd = rs1 << rs2);                                 // sll
+    INSTPAT(L"│0000000     │?????│?????│010   │?????      │0110011│", R, rd = (rs1 < rs2) ? 1 : 0);                        // slt
+    INSTPAT(L"│0000000     │?????│?????│011   │?????      │0110011│", R, rd = ((uint64_t)rs1 < (uint64_t)rs2) ? 1 : 0);    // sltu // CHECK: as unsigned
+    INSTPAT(L"│0000000     │?????│?????│100   │?????      │0110011│", R, rd = rs1 ^ rs2);                                  // xor
+    INSTPAT(L"│0000000     │?????│?????│101   │?????      │0110011│", R, rd = (uint64_t)rs1 >> rs2);                       // srl // CHECK: zero-extend
+    INSTPAT(L"│0100000     │?????│?????│101   │?????      │0110011│", R, rd = rs1 >> rs2);                                 // sra // CHECK: msb-extend
+    INSTPAT(L"│0000000     │?????│?????│110   │?????      │0110011│", R, rd = rs1 | rs2);                                  // or
+    INSTPAT(L"│0000000     │?????│?????│111   │?????      │0110011│", R, rd = rs1 & rs2);                                  // and
+    INSTPAT(L"│0000000     │?????│?????│000   │?????      │0111011│", R, rd = SEXT(BITS(rs1 + rs2, 31, 0), 32));           // addw
+    INSTPAT(L"│0100000     │?????│?????│000   │?????      │0111011│", R, rd = SEXT(BITS(rs1 - rs2, 31, 0), 32));           // subw
+    INSTPAT(L"│0000000     │?????│?????│001   │?????      │0111011│", R, rd = SEXT(BITS(rs1 << rs2, 31, 0), 32));          // sllw
+    INSTPAT(L"│0000000     │?????│?????│101   │?????      │0111011│", R, rd = SEXT(BITS(rs1, 31, 0) >> rs2, 32));          // srlw // CHECK: zero-extend
+    INSTPAT(L"│0100000     │?????│?????│101   │?????      │0111011│", R, rd = SEXT((int32_t)BITS(rs1, 31, 0) >> rs2, 32)); // sraw // CHECK: msb-extend
     // I-type │imm[11:0]         │rs1  │funct3│rd         │opcode │
-    INSTPAT(L"│????????????      │?????│000   │?????      │1100111│", I, rd = s->pc + 4; s->dnpc = rs1 + imm);   // jalr
-    INSTPAT(L"│????????????      │?????│011   │?????      │0000011│", I, rd = Mr(rs1 + imm, 8));                 // ld
-    INSTPAT(L"│????????????      │?????│000   │?????      │0010011│", I, rd = rs1 + imm);                        // addi
-    INSTPAT(L"│000000000001      │00000│000   │00000      │1110011│", I, NEMUTRAP(s->pc, R(10)));                // ebreak, Note: R(10) is $a0
+    INSTPAT(L"│????????????      │?????│000   │?????      │1100111│", I, s->dnpc = (rs1 + imm) & ~1; rd = s->pc + 4;);                 // jalr
+    INSTPAT(L"│????????????      │?????│000   │?????      │0000011│", I, rd = SEXT(BITS(Mr(rs1 + imm, 1), 7, 0), 8));                  // lb
+    INSTPAT(L"│????????????      │?????│001   │?????      │0000011│", I, rd = SEXT(BITS(Mr(rs1 + imm, 2), 15, 0), 16));                // lh
+    INSTPAT(L"│????????????      │?????│010   │?????      │0000011│", I, rd = SEXT(BITS(Mr(rs1 + imm, 4), 31, 0), 32));                // lw
+    INSTPAT(L"│????????????      │?????│011   │?????      │0000011│", I, rd = Mr(rs1 + imm, 8));                                       // ld
+    INSTPAT(L"│????????????      │?????│100   │?????      │0000011│", I, rd = BITS(Mr(rs1 + imm, 1), 7, 0));                           // lbu // CHECK: zero-extend
+    INSTPAT(L"│????????????      │?????│101   │?????      │0000011│", I, rd = BITS(Mr(rs1 + imm, 2), 15, 0));                          // lhu // CHECK: zero-extend
+    INSTPAT(L"│????????????      │?????│000   │?????      │0010011│", I, rd = rs1 + imm);                                              // addi
+    INSTPAT(L"│????????????      │?????│011   │?????      │0010011│", I, rd = ((uint64_t)rs1 < (uint64_t)imm) ? 1 : 0);                // sltiu // CHECK: as unsigned
+    INSTPAT(L"│????????????      │?????│100   │?????      │0010011│", I, rd = rs1 ^ imm);                                              // xori
+    INSTPAT(L"│????????????      │?????│110   │?????      │0010011│", I, rd = rs1 | imm);                                              // ori
+    INSTPAT(L"│????????????      │?????│111   │?????      │0010011│", I, rd = rs1 & imm);                                              // andi
+    INSTPAT(L"│000000??????      │?????│001   │?????      │0010011│", I, rd = rs1 << BITS(imm, 5, 0));                                 // slli // CHECK: shamt = imm[5:0]
+    INSTPAT(L"│000000??????      │?????│101   │?????      │0010011│", I, rd = (uint64_t)rs1 >> BITS(imm, 5, 0));                       // srli // CHECK: zero-extend,  shamt = imm[5:0]
+    INSTPAT(L"│010000??????      │?????│101   │?????      │0010011│", I, rd = rs1 >> BITS(imm, 5, 0));                                 // srai // CHECK: msb-extend,  shamt = imm[5:0]
+    INSTPAT(L"│????????????      │?????│000   │?????      │0011011│", I, rd = SEXT(BITS(rs1 + imm, 31, 0), 32));                       // addiw
+    INSTPAT(L"│0000000?????      │?????│001   │?????      │0011011│", I, rd = SEXT(BITS(rs1 << BITS(imm, 4, 0), 31, 0), 32));          // slliw // CHECK: shamt = imm[4:0]
+    INSTPAT(L"│0000000?????      │?????│101   │?????      │0011011│", I, rd = SEXT(BITS(rs1, 31, 0) >> BITS(imm, 4, 0), 32));          // srliw // CHECK: zero-extend, shamt = imm[4:0]
+    INSTPAT(L"│0100000?????      │?????│101   │?????      │0011011│", I, rd = (int64_t)SEXT(BITS(rs1, 31, 0), 32) >> BITS(imm, 4, 0)); // sraiw // CHECK: msb-extend, shamt = imm[4:0]
+    INSTPAT(L"│000000000001      │00000│000   │00000      │1110011│", I, NEMUTRAP(s->pc, R(10)));                                      // ebreak, Note: R(10) is $a0
     // S-type │imm[11:5]   │rs2  │rs1  │funct3│imm[4:0]   │opcode │
-    INSTPAT(L"│???????     │?????│?????│011   │?????      │0100011│", S, Mw(rs1 + imm, 8, rs2));                 // sd
+    INSTPAT(L"│???????     │?????│?????│000   │?????      │0100011│", S, Mw(rs1 + imm, 1, BITS(rs2, 7, 0)));  // sb
+    INSTPAT(L"│???????     │?????│?????│001   │?????      │0100011│", S, Mw(rs1 + imm, 2, BITS(rs2, 15, 0))); // sh
+    INSTPAT(L"│???????     │?????│?????│010   │?????      │0100011│", S, Mw(rs1 + imm, 4, BITS(rs2, 31, 0))); // sw
+    INSTPAT(L"│???????     │?????│?????│011   │?????      │0100011│", S, Mw(rs1 + imm, 8, rs2));              // sd
     // B-type │imm[12|10:5]│rs2  │rs1  │funct3│imm[4:1|11]│opcode │
+    INSTPAT(L"│???????     │?????│?????│000   │?????      │1100011│", B, if (rs1 == rs2) { s->dnpc = s->pc + imm; });                     // beq
+    INSTPAT(L"│???????     │?????│?????│001   │?????      │1100011│", B, if (rs1 != rs2) { s->dnpc = s->pc + imm; });                     // bne
+    INSTPAT(L"│???????     │?????│?????│100   │?????      │1100011│", B, if (rs1 < rs2) { s->dnpc = s->pc + imm; });                      // blt
+    INSTPAT(L"│???????     │?????│?????│101   │?????      │1100011│", B, if (rs1 >= rs2) { s->dnpc = s->pc + imm; });                     // bge
+    INSTPAT(L"│???????     │?????│?????│110   │?????      │1100011│", B, if ((uint64_t)rs1 < (uint64_t)rs2) { s->dnpc = s->pc + imm; });  // bltu // CHECK: as unsigned
+    INSTPAT(L"│???????     │?????│?????│111   │?????      │1100011│", B, if ((uint64_t)rs1 >= (uint64_t)rs2) { s->dnpc = s->pc + imm; }); // bgeu // CHECK: as unsigned
     // U-type │imm[31:12]                     │rd         │opcode │
-    INSTPAT(L"│????????????????????           │?????      │0010111│", U, rd = s->pc + imm);                      // auipc
+    INSTPAT(L"│????????????????????           │?????      │0110111│", U, rd = imm);         // lui
+    INSTPAT(L"│????????????????????           │?????      │0010111│", U, rd = s->pc + imm); // auipc
     // J-type │imm[20|10:1|11|19:12]          │rd         │opcode │
-    INSTPAT(L"│????????????????????           │?????      │1101111│", J, rd = s->pc + 4; s->dnpc = s->pc + imm); // jal
+    INSTPAT(L"│????????????????????           │?????      │1101111│", J, rd = s->pc + 4; s->dnpc = s->pc + imm;); // jal
+
+    // RV64M Standard Extension
+    // R-type │funct7      │rs2  │rs1  │funct3│rd         │opcode │
+    INSTPAT(L"│0000001     │?????│?????│000   │?????      │0110011│", R, rd = rs1 * rs2);                                                       // mul
+    INSTPAT(L"│0000001     │?????│?????│000   │?????      │0111011│", R, rd = SEXT(BITS(rs1 * rs2, 31, 0), 32));                                // mulw
+    INSTPAT(L"│0000001     │?????│?????│100   │?????      │0111011│", R, rd = SEXT((int32_t)BITS(rs1, 31, 0) / (int32_t)BITS(rs2, 31, 0), 32)); // divw
+    INSTPAT(L"│0000001     │?????│?????│110   │?????      │0111011│", R, rd = SEXT((int32_t)BITS(rs1, 31, 0) % (int32_t)BITS(rs2, 31, 0), 32)); // remw
 
     // If an instruction does not match any pattern of above, it is invalid
     INSTPAT(L"│????????????      │?????│???   │?????      │???????│", I, INV(s->pc));
