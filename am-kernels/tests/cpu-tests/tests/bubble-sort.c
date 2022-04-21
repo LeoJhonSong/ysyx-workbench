ab6e6d0449be0da04616ef63bf2b48f40be28691
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 12:37:27 +0000

    > compile NEMU ysyx_22041024 宋铸恒 Linux YSYX 5.15.32-1-MANJARO #1 SMP PREEMPT Mon Mar 28 09:16:36 UTC 2022 x86_64 GNU/Linux  12:37:27 up 1 day, 10:17,  0 users,  load average: 1.37, 0.97, 0.61

--- am-kernels/tests/cpu-tests/tests/bubble-sort.c
+++ am-kernels/tests/cpu-tests/tests/bubble-sort.c
@@ -1,11 +1,9 @@
 #include "trap.h"
 
-#define N 5
+#define N 20
 
-// int a[N] = {2, 12, 14, 6, 13, 15, 16, 10, 0, 18, 11, 19, 9, 1, 7, 5, 4, 3, 8, 17};
-int a[N] = {4, 3, 2, 1, 0};
+int a[N] = {2, 12, 14, 6, 13, 15, 16, 10, 0, 18, 11, 19, 9, 1, 7, 5, 4, 3, 8, 17};
 
-__attribute__((noinline))
 void bubble_sort() {
 	int i, j, t;
 	for(j = 0; j < N; j ++) {
@@ -27,15 +25,15 @@ int main() {
 		check(a[i] == i);
 	}
 
-	// check(i == N);
+	check(i == N);
 
-	// bubble_sort();
+	bubble_sort();
 
-	// for(i = 0; i < N; i ++) {
-	// 	check(a[i] == i);
-	// }
+	for(i = 0; i < N; i ++) {
+		check(a[i] == i);
+	}
 
-	// check(i == N);
+	check(i == N);
 
 	return 0;
 }
